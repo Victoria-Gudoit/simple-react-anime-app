3915ab9ba25226a120dcef92c6ad0050e80b5a9e
Victoria-Gudoit <vgudojt@mail.ru>
2022-05-12 22:58:26 +0300

    init: start project

--- src/api/items.js
+++ src/api/items.js
@@ -2,7 +2,7 @@ import { request } from "./request";
 
 const HOST = "https://api.jikan.moe/v4/top";
 
-export const fetchItems = (type, search = "") => {
+export const fetchItems = (type) => {
   const url = `${HOST}/${type}`;
   const result = request(url);
   return result;
